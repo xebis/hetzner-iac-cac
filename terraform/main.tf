5df53dc7bb38852ee2c7021aae42020d32082688
Martin Bruzina <martin@bruzina.cz>
2025-04-21 16:22:17 +0200

    feat: add ansible inventory and test playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,11 @@ resource "hcloud_server" "demo" {
   name        = "${terraform.workspace}-demo"
   image       = "ubuntu-24.04"
   server_type = "cax11"
-  user_data   = templatefile("${path.module}/cloud-config.yaml.tftpl", { ssh_id_pub = var.ssh_id_pub})
+  user_data   = templatefile("${path.module}/cloud-config.yaml.tftpl", { ssh_id_pub = var.ssh_id_pub })
+
+  labels = {
+    env = terraform.workspace
+  }
 
   public_net {
     ipv4_enabled = true
