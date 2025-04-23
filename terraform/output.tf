4c252bc9afbef1e848a3b54c5787a2da4229b3b8
Martin Bruzina <martin@bruzina.cz>
2025-04-23 22:01:22 +0200

    fix: terraform ipv4 address output should be empty instead of null

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
 output "ipv4_address" {
-  value       = length(hcloud_server.demo) > 0 ? hcloud_server.demo[0].ipv4_address : null
+  value       = length(hcloud_server.demo) > 0 ? hcloud_server.demo[0].ipv4_address : ""
   description = "Demo public IPv4 address"
 }
