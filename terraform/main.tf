50973d50be6777977b8faf3d84c5e688d55a6595
Martin Bruzina <martin@bruzina.cz>
2025-05-10 11:26:16 +0200

    fix: production should have zero servers created

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "hcloud_server" "demo" {
-  count       = terraform.workspace == "production" ? 0 : 1
+  count       = terraform.workspace == "prod" ? 0 : 1
   name        = "${terraform.workspace}-demo"
   image       = "ubuntu-24.04"
   server_type = "cax11"
